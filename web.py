4fa661fb704a722d2e6d8169b7afa633e492129a
Alex <alexeytimonov1@gmail.com>
2024-12-20 14:46:40 +0200

    implement "Complete" fucntionalities

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit import session_state
+
 import functions
 
 todos = functions.get_todos()
@@ -12,8 +14,13 @@ st.title("My Todo app")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
